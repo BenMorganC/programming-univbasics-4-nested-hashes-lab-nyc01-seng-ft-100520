1064b0ace14182aecfc7a9b8c68e387283bb53e1
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-18 15:44:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,9 +107,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
- 
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
- programmer_hash[:alan_kay][:known_for] = "GUI"
+ 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
